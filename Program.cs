051ff771649f33e7e3d4396e3ee63d2411cf0ced
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 07:48:48 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class program
+{
 
+
+    class PemrosesData
+    {
+        public T DapatkanNilaiTerbesar<T>(T data1, T data2, T data3)
+        {
+            dynamic nilai1 = (dynamic)data1;
+            dynamic nilai2 = (dynamic)data2;
+            dynamic nilai3 = (dynamic)data3;
+
+            dynamic nilaiMax = nilai1;
+            if (nilai2 > nilaiMax)
+            {
+                nilaiMax = nilai2;
+            }
+            if (nilai3 > nilaiMax)
+            {
+                nilaiMax = nilai3;
+            }
+            return nilaiMax;
+        }
+        private static void Main(String[] args)
+        {
+            PemrosesData pemroses = new PemrosesData();
+            double terbesar = pemroses.DapatkanNilaiTerbesar(1.1, 2.2, 3.3);
+            Console.WriteLine(terbesar);
+        }
+
+    }
+}
